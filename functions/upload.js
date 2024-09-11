d9fe80973407e13e078e6aac2ed638fc30ea7a6f
Han <1655466387@qq.com>
2024-09-11 20:12:37 +0800

    优化

--- functions/upload.js
+++ functions/upload.js
@@ -18,7 +18,9 @@ export async function onRequest({ request }) {
     method: 'POST',
     body: data,
     headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36 LBBROWSER'
+      'Content-Type': 'multipart/form-data',
+      Referer: 'https://www.liebao.com/',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
     }
   })
 }
